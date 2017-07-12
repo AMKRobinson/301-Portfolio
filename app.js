334227b9274ec690d8554cf9fc59e2f426b52e68
Anthony Robinson <robinsonamk@gmail.com>
2017-07-12 10:53:40 -0700

    finished all technical requirements for class-01

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 'use strict';
+
+var projects = [];
+
+function project(name, date, body, language, collaborator, image) {
+  this.name = name;
+  this.date = date;
+  this.body = body;
+  this.language = language;
+  this.collaborator = collaborator;
+  this.image = image;
+  projects.push(this);
+}
+
+$(function() {
+  $('.fa-bars').on('click',function(e) {
+    e.preventDefault();
+    $('.list').slideToggle();
+  });
+});
