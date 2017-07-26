170625e1e5bd464841c80e0392ef8413dc1ff83d
Anthony Robinson <robinsonamk@gmail.com>
2017-07-26 11:12:18 -0700

    wrapped my project constructor in an iife

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,62 @@
 'use strict';
+var app = app || {};
 
+(function(module) {
 
+// Project constructor function
+
+  function Project (rawDataObj) {
+    this.title = rawDataObj.title;
+    this.body = rawDataObj.body;
+    this.projectUrl = rawDataObj.projectUrl;
+    this.images = rawDataObj.images;
+  }
+
+  Project.all = [];
+
+// This function selects the project template and compiles the data from the constructor
+
+  Project.prototype.toHtml = function() {
+    var myProjectlist = $('#projectlist-template').html();
+    var compiled = Handlebars.compile(myProjectlist);
+    return compiled(this);
+  };
+
+  Project.loadAll = function(rawData) {
+    console.log('rd: ', rawData);
+    rawData.forEach(function(ele) {
+      console.log('el', ele);
+      Project.all.push(new Project(ele));
+    })
+  };
+
+  function loadProjectsToDom() {
+    Project.all.forEach(function(allprojects) {
+      $('#article').append(allprojects.toHtml());
+    });
+  }
+
+// fetch the json data
+
+  Project.fetchAll = function() {
+    if (localStorage.rawData) {
+      Project.loadAll(JSON.parse(localStorage.rawData));
+      loadProjectsToDom();
+    } else {
+      $.getJSON('data/rawData.json')
+        .then(function(data) {
+          localStorage.setItem('rawData', JSON.stringify(data))
+          console.log('Winning!', data)
+          Project.loadAll(JSON.parse(localStorage.rawData))
+          loadProjectsToDom();
+        }, function(err) {
+          console.log('Doh!', err)
+        })
+    }
+  }
+  module.Project = Project;
+  Project.fetchAll();
+})(app)
 // Hamburger icon animation & show nav
 
 $(function() {
@@ -23,56 +79,3 @@ $('.tab').on('click', function(e) {
   var sectionTarget = clickedTab.dataset.content;
   $('#' + sectionTarget).css('display', 'block');
 });
-
-// Project constructor function
-
-function Project (rawDataObj) {
-  this.title = rawDataObj.title;
-  this.body = rawDataObj.body;
-  this.projectUrl = rawDataObj.projectUrl;
-  this.images = rawDataObj.images;
-}
-
-Project.all = [];
-
-// This function selects the project template and compiles the data from the constructor
-
-Project.prototype.toHtml = function() {
-  var myProjectlist = $('#projectlist-template').html();
-  var compiled = Handlebars.compile(myProjectlist);
-  return compiled(this);
-};
-
-Project.loadAll = function(rawData) {
-  console.log('rd: ', rawData);
-  rawData.forEach(function(ele) {
-    console.log('el', ele);
-    Project.all.push(new Project(ele));
-  })
-};
-
-function loadProjectsToDom() {
-  Project.all.forEach(function(potato) {
-    $('#article').append(potato.toHtml());
-  });
-}
-
-// fetch the json data
-
-Project.fetchAll = function() {
-  if (localStorage.rawData) {
-    Project.loadAll(JSON.parse(localStorage.rawData));
-    loadProjectsToDom();
-  } else {
-    $.getJSON('data/rawData.json')
-      .then(function(data) {
-        localStorage.setItem('rawData', JSON.stringify(data))
-        console.log('Winning!', data)
-        Project.loadAll(JSON.parse(localStorage.rawData))
-        loadProjectsToDom();
-      }, function(err) {
-        console.log('Doh!', err)
-      })
-  }
-}
-Project.fetchAll();
--- server.js
+++ server.js
@@ -10,18 +10,9 @@ const app = express();
 
 app.use(express.static('.'));
 
-// app.get('*', function(request, response) {
-//   response.sendFile(index.html {root: .})
-// })
-
 app.get('/index', function(request, response) {
   response.sendFile('index.html', {root: '.'});
 });
-
-// app.post('/article', bodyParser, function(request, response) {
-//   console.log(request.body);
-//   response.send('stuff');
-// })
 
 app.listen(PORT, function() {
   console.log(`The server has started using this port:" ${PORT}"`);
